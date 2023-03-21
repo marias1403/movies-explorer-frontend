5fcc7ed53b622b2fb6b1b79d7f9841fc9479fe7c
Maria Arkhipova <marias1403@yandex.ru>
2023-03-21 22:08:39 +0900

    fix after review

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,19 +3,19 @@ import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className='header page__header section'>
+    <header className='header header_authorized page__header section'>
       <div className='header__wrapper'>
         <div className='header__logo-container'>
           <NavLink className='header__logo link' to='/'></NavLink>
-          <div className='header__nav-container header__nav-container_hidden'>
+          <div className='header__nav-container'>
             <NavLink to='/movies' className='header__link link'>Фильмы</NavLink>
             <NavLink to='/saved-movies' className='header__link link'>Сохранённые фильмы</NavLink>
           </div>
         </div>
         <div className='header__container'>
-          <NavLink to='/signup' className='header__link link'>Регистрация</NavLink>
-          <NavLink to='/signin' className='header__button link'>Войти</NavLink>
-          <div className='header__account-wrapper header__account-wrapper_hidden'>
+          <NavLink to='/signup' className='header__link header__link_hidden link'>Регистрация</NavLink>
+          <NavLink to='/signin' className='header__button header__button_hidden link'>Войти</NavLink>
+          <div className='header__account-wrapper'>
             <NavLink to='/profile' className='header__link link'>Аккаунт</NavLink>
             <div className='header__link-icon link'></div>
           </div>
